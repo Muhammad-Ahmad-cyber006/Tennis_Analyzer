00134da21ef662079fad638c6041d3321a5350d0
Muhammad-Ahmad-cyber006 <novahat2580@gmail.com>
2026-07-06 01:37:42 +0500

    Add utils folder

---
+++ utils/video_utils.py
@@ -0,0 +1,19 @@
+import cv2
+def read_video(video_path):
+    cap = cv2.VideoCapture(video_path)
+    frames = []
+    while True:
+        ret, frame = cap.read()
+        if not ret:  # ret False matlab video khatam ya frame read fail
+            break
+        frames.append(frame)
+    cap.release()
+    return frames
+
+
+def save_video(output_video_frames, output_video_path):
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')  # codec for .avi
+    out = cv2.VideoWriter(output_video_path, fourcc, 24, (output_video_frames[0].shape[1], output_video_frames[0].shape[0]))  # 24 fps hardcoded, shape se width/height le rahe
+    for frame in output_video_frames:
+        out.write(frame)
+    out.release()
